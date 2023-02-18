3ccb3eb90590d65e01fe9b17cea6746bd914d1c2
Tesfay-Teshome <tesfix2017@gmail.com>
2023-02-18 13:22:51 +0300

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -17,11 +17,11 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 class Server:
     """ Server class to paginate a database of popular baby names. """
     DATA_FILE = "Popular_Baby_Names.csv"
-    
+
     def __init__(self):
         """ Initializes a new Server instance."""
         self.__dataset = None
-        
+
     def dataset(self) -> List[List]:
         """ Cached dataset """
         if self.__dataset is None:
@@ -29,8 +29,9 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-        
+
         return self.__dataset
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ Retrieves a page of data."""
         assert type(page) == int and type(page_size) == int
@@ -40,7 +41,7 @@ class Server:
         if start_point > len(data):
             return []
         return data[start_point: end_point]
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """ Retrieves information about a page."""
         page_data = self.get_page(page, page_size)
