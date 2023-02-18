b006a2629ffd9004a96b3de4b39f3f36b860c2ce
Tesfay-Teshome <tesfix2017@gmail.com>
2023-02-18 12:27:25 +0300

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -18,9 +18,11 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
-    def dataset(self) -> List[List]:  # sourcery skip: identity-comprehension
+    def dataset(self) -> List[List]:
         """Cached dataset
         """
         if self.__dataset is None:
@@ -32,16 +34,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''
-            Returns a page of data.
-        '''
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        self.dataset()
-
-        if self.dataset() is None:
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start_point, end_point = index_range(page, page_size)
+        data = self.dataset()
+        if start_point > len(data):
             return []
-
-        indexRange = index_range(page, page_size)
-        return self.dataset()[indexRange[0]:indexRange[1]]
+        return data[start_point:end_point]
     
