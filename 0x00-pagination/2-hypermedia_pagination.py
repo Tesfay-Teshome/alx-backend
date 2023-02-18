4c77d060c92aab028a15e9de673dd348d174cda0
Tesfay-Teshome <tesfix2017@gmail.com>
2023-02-18 13:38:51 +0300

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -40,28 +40,24 @@ class Server:
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
+        start_point, end_point = index_range(page, page_size)
         data = self.dataset()
-        if start > len(data):
+        if start_point > len(data):
             return []
-<<<<<<< HEAD
-        return data[start:end]
-=======
-        return data[start_point: end_point]
->>>>>>> 3ccb3eb90590d65e01fe9b17cea6746bd914d1c2
+        return data[start_point:end_point]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """Retrieves information about a page.
         """
         page_data = self.get_page(page, page_size)
-        start, end = index_range(page, page_size)
+        start_point, end_point = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
         page_info = {
             'page_size': len(page_data),
             'page': page,
             'data': page_data,
-            'next_page': page + 1 if end < len(self.__dataset) else None,
-            'prev_page': page - 1 if start > 0 else None,
+            'next_page': page + 1 if end_point < len(self.__dataset) else None,
+            'prev_page': page - 1 if start_point > 0 else None,
             'total_pages': total_pages,
         }
         return page_info
